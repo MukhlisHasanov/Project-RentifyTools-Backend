e39936b52b2514ae92b9342e14faa3a61599bdc2
AndreyHein <soly.a@web.de>
2024-11-10 09:59:54 +0100

    update for Swagger

--- rentify-tools/src/main/java/org/rentifytools/dto/userDto/UserResponseDto.java
+++ rentify-tools/src/main/java/org/rentifytools/dto/userDto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package org.rentifytools.dto.userDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,23 @@ import java.util.Set;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "ResponseDTO class for User")
 public class UserResponseDto {
+    @Schema(description = "User ID", example = "1")
     private Long id;
+
+    @Schema(description = "User firstname", example = "John")
     private String firstname;
+
+    @Schema(description = "User lastname", example = "Johnson")
     private String lastname;
+
+    @Schema(description = "User email", example = "john@example.com")
     private String email;
+
+    @Schema(description = "User phone number", example = "123-456-789")
     private String phone;
+
+    @Schema(description = "User status", example = "USER")
     private Set<RoleResponseDto> roles;
 }
--- rentify-tools/src/main/java/org/rentifytools/entity/User.java
+++ rentify-tools/src/main/java/org/rentifytools/entity/User.java
@@ -19,7 +19,6 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @NotBlank
