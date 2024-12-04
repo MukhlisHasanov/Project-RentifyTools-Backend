16ae7a58fbbf07695e66e35f6f548f14a2514834
ilofrudd <boyko5000@gmail.com>
2024-12-04 16:43:34 +0100

    sss

--- src/main/java/org/rentifytools/entity/ToolImage.java
+++ src/main/java/org/rentifytools/entity/ToolImage.java
@@ -1,5 +1,6 @@
 package org.rentifytools.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
