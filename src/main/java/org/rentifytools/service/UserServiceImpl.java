0db7624dc81307c56e66df1fbed913f6ba1238cf
bush <anton.gorbovyi@gmail.com>
2024-12-15 13:30:49 +0100

    some changed

--- src/main/java/org/rentifytools/service/UserServiceImpl.java
+++ src/main/java/org/rentifytools/service/UserServiceImpl.java
@@ -45,18 +45,17 @@ public class UserServiceImpl implements UserService {
 
         User user = mapper.map(dto, User.class);
 
+        user.setPassword(encoder.encode(dto.getPassword()));
+        user.setRoles(Set.of(roleService.getRole("USER")));
+
 //        if (dto.getAddress() != null) {
         Address address = mapper.map(dto.getAddress(), Address.class);
         Address savedAddress = addressRepository.save(address);
         user.setAddress(savedAddress);
-//        }
-
-        user.setPassword(encoder.encode(dto.getPassword()));
-        user.setRoles(Set.of(roleService.getRole("USER")));
 
         User savedUser = repository.save(user);
         savedAddress.setUser(savedUser);
-
+//        }
         return mapper.map(savedUser, UserResponseDto.class);
     }
 
